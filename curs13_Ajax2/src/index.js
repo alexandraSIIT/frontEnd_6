f3f1107221e9fa30759e8f76f7a429d82d57caf5
alexandraSIIT <alexandra.vilcan@scoalainformala.ro>
2018-10-10 22:55:00 +0300

    Added edit article and get comments functionality for Ajax2

--- curs13_Ajax2/src/index.js
+++ curs13_Ajax2/src/index.js
@@ -36,11 +36,69 @@ $.ajax({
         deleteArticle(event.target);
       });
 
+      var editButton = articleClone.querySelector(".article-edit");
+      // this sends the event as parameter to updateArticleOnClick function
+      editButton.addEventListener("click", updateArticleOnClick);
+
+      getArticleComments(response[i].id);
+
       // append the clone to the articles container
       articlesContainer.appendChild(articleClone);
     }
   }
 });
+
+function updateArticleOnClick(event) {
+  // event.target is the clicked button
+  // disable the button so we can't click it several times
+  event.target.disabled = true;
+
+  var grandpa = event.target.parentNode.parentNode; // the article
+  var grandpaId = grandpa.id;
+  var articleId = grandpaId.replace("article_", "");
+
+  var inputTitle = document.createElement("input");
+  inputTitle.setAttribute("class", "new-title");
+  inputTitle.setAttribute("style", "width: 80%"); // the default width is too small
+  inputTitle.value = grandpa.querySelector(".article-title").innerText;
+  grandpa.appendChild(inputTitle);
+
+  var inputBody = document.createElement("textarea");
+  inputBody.setAttribute("class", "new-body");
+  inputBody.setAttribute("style", "width: 80%");
+  inputBody.value = grandpa.querySelector(".article-body").innerText;
+  grandpa.appendChild(inputBody);
+
+  var updateButton = document.createElement("button");
+  updateButton.innerText = "Update";
+  grandpa.appendChild(updateButton);
+
+  updateButton.addEventListener("click", function() {
+    $.ajax({
+      url: articlesRootUrl + "posts/" + articleId,
+      method: "PATCH",
+      data: {
+        title: inputTitle.value,
+        body: inputBody.value
+      },
+      success: function(response) {
+        console.log("edited", response);
+
+        // populate items with the response from the API
+        grandpa.querySelector(".article-title").innerText = response.title;
+        grandpa.querySelector(".article-body").innerText = response.body;
+
+        // remove input and textarea created by us
+        inputTitle.remove();
+        inputBody.remove();
+        updateButton.remove();
+
+        // enable back the "Edit" button
+        event.target.disabled = false;
+      }
+    });
+  });
+}
 
 function deleteArticle(clickedButton) {
   console.log("clicked button", clickedButton);
@@ -57,4 +115,16 @@ function deleteArticle(clickedButton) {
       grandpa.remove();
     }
   });
+}
+
+function getArticleComments(articleId) {
+  var url = articlesRootUrl + "posts" + articleId + "/comments";
+
+  try {
+    $.get(url, function(response, error) {
+      console.log("comments", response, error);
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
 }
