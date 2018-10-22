cc5352a0a48ad6842887fa9d3f3474199daa3854
alexandraSIIT <alexandra.vilcan@scoalainformala.ro>
2018-10-22 23:15:38 +0300

    Added "proper" error handling to AJAX requests using both $.ajax & Fetch

--- curs14_Promises/src/apiCalls.js
+++ curs14_Promises/src/apiCalls.js
@@ -1,17 +1,49 @@
+// There are two approaches for handling errors when using (jQuery) AJAX:
+// 1. using catch in the promise functions chain - see getArticleComments below
+// 2. using the second argument of the then function - see deleteArticle below
+
 var rootUrl = "https://jsonplaceholder.typicode.com/";
 
 function getArticleComments(articleId) {
-  var url = rootUrl + "posts/" + articleId + "/comments";
-  return $.get(url);
+  var url = rootUrl + "posts/" + articleId + "comments"; // intentionally incorrect
+  return (
+    $
+      .get(url)
+      // if an error occurs, the catch function will "catch" that error and will use it to display it to the user
+      .catch(function(error) {
+        console.log(
+          "GET Comments request was rejected with status ",
+          error.status
+        );
+      })
+  );
 }
 
 function deleteArticle(articleId) {
   // DO NOT FORGET TO RETURN THE PROMISE!!!
   // AJAX calls always create (automatically) a promise
-  return $.ajax({
-    url: rootUrl + "posts" + articleId,
-    method: "DELETE"
-  });
+  return $
+    .ajax({
+      url: rootUrl + "posts" + articleId,
+      method: "DELETE"
+    })
+    .then(
+      function(response) {
+        // = the first argument of then
+        // if API call is successful, process the response and then return it / the results of the processing
+        // note that here the response is already in JSON format, and is the actual response from the API (unlike with Fetch)
+        console.log(response);
+        return response;
+      },
+      function(error) {
+        // = the second argument of then
+        // if API call is not successful, "process" the error (in our case, log it in the console)
+        console.log(
+          "DELETE article request was rejected with status ",
+          error.status
+        );
+      }
+    );
 }
 
 function updateArticle(articleId, inputTitle, inputBody) {
--- curs15_Fetch/src/apiCalls.js
+++ curs15_Fetch/src/apiCalls.js
@@ -1,17 +1,57 @@
+// When using Fetch API, only the "server network errors" are caught using .catch, i.e.,
+// when the server cannot be reached: connection refused or name not resolved.
+// To fix that, use the response.ok property - see below in getArticleComments function
+
 var rootUrl = "https://jsonplaceholder.typicode.com/";
 
 function getArticleComments(articleId) {
-  var url = rootUrl + "posts/" + articleId + "/comments";
+  var url = rootUrl + "posts/" + articleId + "comments"; // intentionally incorrect
   // return $.get(url);
 
-  return fetch(url, { method: "GET" })
-    .then(function(response) {
-      // deserialize response (turn string into json)
-      return response.json();
-    })
-    .catch(function(error) {
-      console.log("ERROR!!!", error);
-    });
+  return (
+    fetch(url, { method: "GET" })
+      .then(function(response) {
+        console.log("response", response);
+        // if it's an error (see URL below), response looks like this:
+        // body: (...) //ReadableStream
+        // bodyUsed: false
+        // headers: Headers { }
+        // ok: false
+        // redirected: false
+        // status: 404
+        // statusText: ""
+        // type: "cors"
+        // url: "https://jsonplaceholder.typicode.com/posts/1comments"
+
+        // if there is no error (again, see URL below), response looks like this:
+        // body: (...) //ReadableStream
+        // bodyUsed: true
+        // headers: Headers { }
+        // ok: true
+        // redirected: false
+        // status: 200
+        // statusText: ""
+        // type: "cors"
+        // url: "https://jsonplaceholder.typicode.com/posts/1/comments"
+
+        if (response.ok) {
+          // deserialize response (turn string into json)
+          return response.json();
+        } else {
+          throw Error(
+            "GET Comments request was rejected with status " + response.status
+          );
+        }
+      })
+      // The catch below will not be triggered by itself, unless the server cannot be reached: connection refused or name not resolved.
+      // This only happens with "Fetch API", not also with jQuery ajax (or other similar libraries).
+      // However, it will be triggered if the response.ok is verified and the error is thrown (as done above)
+      .catch(function(error) {
+        console.log("ERROR:", error);
+      })
+  );
+
+  // The code below was just a test to prove that what we return here is the input to the next function in the chain
   // .then(function(response) {
   //   console.log("api response", response);
   //   return "facem pauza";
@@ -24,16 +64,17 @@ function deleteArticle(articleId) {
   return fetch(rootUrl + "posts" + articleId, {
     method: "DELETE"
   })
-    .then(
-      function(response) {
+    .then(function(response) {
+      if (response.ok) {
         return response.json();
-      },
-      function(error) {
-        console.log("ERROR in then!!!", error);
+      } else {
+        throw Error(
+          "DELETE article request was rejected with status " + response.status
+        );
       }
-    )
+    })
     .catch(function(error) {
-      console.log("ERROR!!!", error);
+      console.log("ERROR:", error);
     });
 }
 
@@ -44,11 +85,33 @@ function updateArticle(articleId, inputTitle, inputBody) {
       title: inputTitle.value,
       body: inputBody.value
     })
-  });
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error(
+          "UPDATE article request was rejected with status " + response.status
+        );
+      }
+    })
+    .catch(function(error) {
+      console.log("ERROR:", error);
+    });
 }
 
 function getArticles() {
-  return fetch(rootUrl + "posts").then(function(response) {
-    return response.json();
-  });
+  return fetch(rootUrl + "posts")
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error(
+          "GET articles request was rejected with status " + response.status
+        );
+      }
+    })
+    .catch(function(error) {
+      console.log("ERROR:", error);
+    });
 }
